c310eaf7a6384cb9ca19c67407f399ddd3b84f6e
lynnliu655 <66851922+lynnliu655@users.noreply.github.com>
2020-08-24 13:47:26 -0700

    resolve review issues

--- GpsDataCapturer/sharedlibrary/src/main/java/com/google/sharedlibrary/model/GpsData.java
+++ GpsDataCapturer/sharedlibrary/src/main/java/com/google/sharedlibrary/model/GpsData.java
@@ -6,50 +6,97 @@ import androidx.annotation.NonNull;
 
 import java.text.DecimalFormat;
 
-/**
- * The data class for GPS data, which will be passed to UI via {@link GpsInfoViewModel}
- */
+/** The data class for GPS data, which will be passed to UI via {@link GpsInfoViewModel} */
 public class GpsData {
-    private String latitude;
-    private String longitude;
-    private String speed;
-    private DecimalFormat locationDF= new DecimalFormat("0.000000");
-    private DecimalFormat speedDF = new DecimalFormat("0.0000");
+  private final String latitude;
+  private final String longitude;
+  private final String speed;
+  private final DecimalFormat locationDF = new DecimalFormat("0.000000");
+  private final DecimalFormat speedDF = new DecimalFormat("0.0000");
 
-    /**
-     * Constructor to create a GpsData object
-     * @param location
-     */
-    public GpsData(Location location) {
-        latitude = String.valueOf(locationDF.format(location.getLatitude()));
-        longitude = String.valueOf(locationDF.format(location.getLongitude()));
-        speed = String.valueOf(location.hasSpeed() ? speedDF.format(location.getSpeed()) : "0.0000");
+  /**
+   * Constructor to create a GpsData object
+   *
+   * @param location the location passed from gpsInfoViewModel
+   */
+  public GpsData(Location location) {
+    latitude = String.valueOf(locationDF.format(location.getLatitude()));
+    longitude = String.valueOf(locationDF.format(location.getLongitude()));
+    speed = String.valueOf(location.hasSpeed() ? speedDF.format(location.getSpeed()) : "0.0000");
+  }
+
+  /**
+   * Get the latitude of the GpsData
+   *
+   * @return latitude
+   */
+  @NonNull
+  public String getLatitude() {
+    return this.latitude;
+  }
+
+  /**
+   * Get the longitude of the GpsData
+   *
+   * @return longitude
+   */
+  @NonNull
+  public String getLongitude() {
+    return this.longitude;
+  }
+
+  /**
+   * Get the speed of the GpsData
+   *
+   * @return speed
+   */
+  @NonNull
+  public String getSpeed() {
+    return this.speed;
+  }
+
+  /**
+   * Get the string of GpsData
+   *
+   * @return a string of GpsData
+   */
+  @NonNull
+  @Override
+  public String toString() {
+    StringBuilder gpsDataBuilder = new StringBuilder();
+    gpsDataBuilder
+        .append("GPS DATA \n")
+        .append("Lat: ")
+        .append(latitude)
+        .append("\n")
+        .append("Lon: ")
+        .append(longitude)
+        .append("\n")
+        .append("Speed: ")
+        .append(speed)
+        .append("\n");
+    return gpsDataBuilder.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
 
-    /**
-     * Get the latitude of the GpsData
-     * @return latitude
-     */
-    @NonNull
-    public String getLatitude(){
-        return this.latitude;
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
     }
 
-    /**
-     * Get the longitude of the GpsData
-     * @return longitude
-     */
-    @NonNull
-    public String getLongitude(){
-        return this.longitude;
-    }
+    GpsData gpsData = (GpsData) o;
 
-    /**
-     * Get the speed of the GpsData
-     * @return speed
-     */
-    @NonNull
-    public String getSpeed(){
-        return this.speed;
-    }
+    return this.latitude.equals(gpsData.latitude)
+        && this.longitude.equals(gpsData.longitude)
+        && this.speed.equals(gpsData.speed);
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
 }
--- GpsDataCapturer/sharedlibrary/src/test/java/com/google/sharedlibrary/GpsDataUnitTest.java
+++ GpsDataCapturer/sharedlibrary/src/test/java/com/google/sharedlibrary/GpsDataUnitTest.java
@@ -1,6 +1,9 @@
 package com.google.sharedlibrary;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import android.location.Location;
@@ -15,23 +18,65 @@ import java.text.DecimalFormat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GpsDataUnitTest {
-    private GpsData gpsData;
-    private DecimalFormat locationDF;
-    private DecimalFormat speedDF;
+  private GpsData gpsData;
+  private DecimalFormat locationDF;
+  private DecimalFormat speedDF;
 
-    @Test
-    public void testGetGpsDataString() {
-        //Given
-        Location location = mock(Location.class);
-        locationDF= new DecimalFormat("0.000000");
-        speedDF = new DecimalFormat("0.0000");
+  @Test
+  public void testGpsData() {
+    // Given
+    Location location = mock(Location.class);
+    locationDF = new DecimalFormat("0.000000");
+    speedDF = new DecimalFormat("0.0000");
 
-        //When
-        gpsData = new GpsData(location);
+    // When
+    gpsData = new GpsData(location);
 
-        //Then
-        assertEquals(locationDF.format(location.getLatitude()), gpsData.getLatitude());
-        assertEquals(locationDF.format(location.getLongitude()), gpsData.getLongitude());
-        assertEquals(speedDF.format(location.getSpeed()), gpsData.getSpeed());
-    }
+    // Then
+    assertEquals(locationDF.format(location.getLatitude()), gpsData.getLatitude());
+    assertEquals(locationDF.format(location.getLongitude()), gpsData.getLongitude());
+    assertEquals(speedDF.format(location.getSpeed()), gpsData.getSpeed());
+  }
+
+  @Test
+  public void testGpsDataToString() {
+    // Given
+    String expected =
+        "GPS DATA \n" + "Lat: " + "0.000000\n" + "Lon: " + "0.000000\n" + "Speed: " + "0.0000\n";
+    Location location = mock(Location.class);
+
+    // When
+    gpsData = new GpsData(location);
+
+    // Then
+    assertEquals(expected, gpsData.toString());
+  }
+
+  @Test
+  public void testGpsDataEqual() {
+    // Given
+    Location location = mock(Location.class);
+    Location location1 = mock(Location.class);
+
+    // When
+    gpsData = new GpsData(location);
+    GpsData gpsData1 = new GpsData(location1);
+
+    // Then
+    assertTrue(gpsData.equals(gpsData1));
+  }
+
+  @Test
+  public void testGpsDataHashCode() {
+    // Given
+    Location location = mock(Location.class);
+    Location location1 = mock(Location.class);
+
+    // When
+    gpsData = new GpsData(location);
+    GpsData gpsData1 = new GpsData(location1);
+
+    // Then
+    assertNotEquals(gpsData.hashCode(), gpsData1.hashCode());
+  }
 }
